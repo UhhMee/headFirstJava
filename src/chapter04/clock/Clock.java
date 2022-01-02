5bdbf430c172bb8c3a42ae93094924244a5ab2ee
UhMe <alex90barinov@gmai.com>
2022-01-02 11:28:32 +0300

    add class Clock

---
+++ src/chapter04/clock/Clock.java
@@ -0,0 +1,14 @@
+package chapter04.clock;
+
+public class Clock {
+
+    private String time;
+
+    public void setTime(String time){
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ src/chapter04/clock/ClockTestDrive.java
@@ -0,0 +1,11 @@
+package chapter04.clock;
+
+public class ClockTestDrive {
+    public static void main(String[] args) {
+
+        Clock c = new Clock();
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("время: " + tod);
+    }
+}
